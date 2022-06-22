e1e6e4d71bedff5b076fbaa8d19959b8c3b11e41
Aaron S Grincewicz <veganimus@outlook.com>
2022-06-22 19:14:24 -0400

    Character dies

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -74,6 +74,12 @@ float AShooterCharacter::TakeDamage(float DamageAmount, FDamageEvent const& Dama
 	return DamageToApply;
 }
 
+bool AShooterCharacter::IsDead() const
+{
+	return Health <= 0;
+}
+
+
 
 
 void AShooterCharacter::MoveForward(float AxisValue)
